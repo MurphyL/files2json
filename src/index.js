d2f0843f0ac7a03229ed83b326252bf92871933c
MurphyL <murphyl@outlook.com>
2021-11-24 19:13:52 +0800

    API 完善

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const rw = require('rw');
 const arg = require('arg');
 const path = require('path');
 const toml = require('@iarna/toml');
@@ -9,12 +8,14 @@ const matter = require('gray-matter');
 const recursive = require("recursive-readdir-sync");
 
 const args = arg({
+    '--out': String,
     '--help': Boolean,
     '--pretty': Boolean,            // JSON 格式化后输出
     '--verbose': Boolean,           // 展示操作日志
     '--display': Boolean,           // 仅用于显示
     '--no-content': Boolean,        // 没有内容
     // Aliases
+    '-o': '--out',
     '-p': '--pretty',
     '-v': '--verbose',
     '-d': '--display',
@@ -23,26 +24,16 @@ const args = arg({
 const debug = args['--verbose'];
 
 if (args['--help']) {
-    const text = rw.readFileSync(path.resolve(process.cwd(), 'src/doc.txt'));
+    const text = fs.readFileSync(path.resolve(process.cwd(), 'src/doc.txt'));
     console.log(text.toString());
     return process.exit(0);
 }
 
-const { _: dirs = [] } = args;
+const files = args['_'];
 
-if (dirs.length < 2) {
-    return console.error('请指定输出文件：', dirs);
+if (files.length === 0) {
+    return console.error('请指定输出文件：', files);
 }
-
-const target = path.resolve(process.cwd(), dirs.pop());
-
-const extensions = ['.toml', '.md'];
-
-const JSON_EXT = '.json';
-
-const ignore = (file) => {
-    return extensions.includes(path.extname(file));
-};
 
 const matterOptions = {
     engines: {
@@ -50,60 +41,62 @@ const matterOptions = {
     }
 };
 
-const tree = { files: {}, items: [] };
+const out = args['--out'];
 
-dirs.forEach(dir => {
-    const root = path.resolve(process.cwd(), dir);
-    const files = recursive(root).filter(ignore);
-    tree.files[root] = files;
-    debug && console.log(root, files);
+const readFile = (filepath) => {
+    const text = fs.readFileSync(filepath);
+    switch (path.extname(filepath)) {
+        case '.md':
+            const { data, content, ...extra } = matter(text.toString(), matterOptions);
+            return {
+                ...data,
+                ...extra,
+                __type: 'markdown',
+                __path: filepath,
+                content: content.trim(),
+            };
+        case '.toml':
+            return {
+                __type: 'toml',
+                __path: filepath,
+                ...toml.parse(text.toString()),
+            };
+        default:
+            return {
+                __type: 'other',
+                __path: filepath,
+                content: text.toString(),
+            };
+    }
+};
+
+const readFiles = (files = []) => {
+    const temp = {};
     files.forEach(file => {
-        const text = rw.readFileSync(file);
-        if (text instanceof Error) {
-            debug && console.log('读取数据文件出错：', file);
-            throw text;
+        const target = path.resolve(process.cwd(), file);
+        if (!temp[file]) { 
+            temp[file] = [];
         }
-        const location = path.relative(process.cwd(), file);
-        switch (path.extname(file)) {
-            case '.md':
-                const { data, content, ...extra } = matter(text.toString(), matterOptions);
-                tree.items.push({
-                    ...data,
-                    ...extra,
-                    __type: 'markdown',
-                    __path: location,
-                    content: content.trim(),
-                });
-                break;
-            case '.toml':
-                tree.items.push({
-                    __type: 'toml',
-                    __path: location,
-                    ...toml.parse(text.toString()),
-                });
-                break;
-            default:
-                tree.items.push({
-                    __type: 'other',
-                    __path: location,
-                    content: text.toString(),
-                });
-                break;
+        if (!fs.existsSync(target)) {
+            return console.warn('Path not exists:', target);
+        }
+        if (fs.lstatSync(target).isDirectory()) {
+            const items = recursive(target);
+            items.forEach(item => {
+                temp[file].push(readFile(item));
+            });
+        } else if (fs.lstatSync(target).isFile()) {
+            temp[file].push(readFile(target));
         }
     });
-});
-
-if (!fs.existsSync(target) && path.extname(target) !== JSON_EXT) {
-    if (path.extname(target) === JSON_EXT) {
-        const parent = path.dirname(target);
-        fs.mkdirSync(parent, { recursive: true });
-        debug && console.log(`output path created: ${parent}\n`);
-    } else {
-        fs.mkdirSync(target, { recursive: true });
-        debug && console.log(`output path created: ${target}\n`);
-    }
+    return temp;
 }
 
-const temp = (path.extname(target) === JSON_EXT) ? target : path.resolve(target, 'merged.json');
+const output = readFiles(files);
 
-rw.writeFileSync(temp, JSON.stringify(tree, null, 4));
+if (out) {
+    const target = path.resolve(process.cwd(), out);
+    fs.writeFileSync(target, JSON.stringify(output, null, 2));
+} else {
+    debug && console.log(output);
+}
